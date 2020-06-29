d2cf0191f007e11fdfa4a7c0a0d41b54ca8cd140
heinekenbh <santos.kaisergabriel@icloud.com>
2020-06-29 10:16:15 -0300

    Index config

---
+++ src/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.section`
+    width: 100%;
+`
+
+export const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const PagesContainer = styled.section`
+    width: 70%;    
+`
